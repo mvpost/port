3ebedc6dd484a7eac5b9f0c66a6b5adec6c91544
mvpostnikov <mvpost@bk.tu>
2023-07-03 13:02:33 +0300

    file upload

--- src/main/java/ru/mtsbank/port/controller/JettyController.java
+++ src/main/java/ru/mtsbank/port/controller/JettyController.java
@@ -3,13 +3,18 @@ package ru.mtsbank.port.controller;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.mtsbank.port.dao.JettyDao;
 import ru.mtsbank.port.entity.Jetty;
 import ru.mtsbank.port.request.ShipRequest;
 import ru.mtsbank.port.service.JettyService;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -47,5 +52,15 @@ public class JettyController {
         return success
                 ? new ResponseEntity<>(HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(value = "/jetty/upload", method = RequestMethod.POST,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File("/var/tmp/"+file.getOriginalFilename());
+        FileOutputStream fileOutputStream = new FileOutputStream(convertFile);
+        fileOutputStream.write(file.getBytes());
+        fileOutputStream.close();
+        return "File is upload successfully";
     }
 }
