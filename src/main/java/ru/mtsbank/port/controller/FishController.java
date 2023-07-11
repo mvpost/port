eef612c484e552652e8a6dc7f9d0b70c4cf26bd7
mvpostnikov <mvpost@bk.tu>
2023-07-11 19:30:08 +0300

    fix

--- src/main/java/ru/mtsbank/port/controller/FishController.java
+++ src/main/java/ru/mtsbank/port/controller/FishController.java
@@ -1,13 +1,8 @@
 package ru.mtsbank.port.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.mtsbank.port.dto.FishDto;
 import ru.mtsbank.port.entity.Fish;
-import ru.mtsbank.port.mapper.FishMapper;
-import ru.mtsbank.port.dto.FishRequestDto;
 import ru.mtsbank.port.service.FishService;
 
 import java.util.List;
@@ -16,24 +11,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FishController {
     private final FishService fishService;
-    private final FishMapper fishMapper;
 
-    @GetMapping(value = "/fishes")
-    public ResponseEntity<List<Fish>> read() {
-        final List<Fish> fish = fishService.readAll();
-        return fish != null
-                ? new ResponseEntity<>(fish, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/fishes")
+    List<Fish> read() {
+        return fishService.readAll();
     }
 
-    @PostMapping (value = "/fishes/cost")
-    public ResponseEntity<FishDto> cost(@RequestBody FishRequestDto fishRequestDto) {
-        final float fishCost = fishService.calcCost(fishRequestDto.name, fishRequestDto.count);
-        FishDto fishDto = fishMapper.map(fishRequestDto);
-        fishDto.setCost(fishCost);
-        return fishCost > 0
-                ? new ResponseEntity<>(fishDto, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping ("/fishes/cost")
+    Float cost(@RequestParam("fishName") String fishName, @RequestParam("fishCount") String fishCount) {
+        return fishService.calcCost(fishName, Float.parseFloat(fishCount));
     }
 
 }
