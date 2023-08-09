aff5e4e8813f1b8550e3ccdb6ce5f0e1ffc17b5e
mvpostnikov <mvpost@bk.tu>
2023-08-09 16:37:16 +0300

    fix

--- src/main/java/ru/mtsbank/port/controller/FishController.java
+++ src/main/java/ru/mtsbank/port/controller/FishController.java
@@ -1,6 +1,10 @@
 package ru.mtsbank.port.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.mtsbank.port.dto.FishDto;
 import ru.mtsbank.port.mapper.FishMapper;
@@ -9,18 +13,21 @@ import ru.mtsbank.port.service.FishService;
 import java.util.List;
 
 @RestController
+@RequestMapping("/fishes")
 @RequiredArgsConstructor
+@Validated
 public class FishController {
     private final FishService fishService;
     private final FishMapper fishMapper;
 
-    @GetMapping("/fishes")
+    @GetMapping()
     List<FishDto> read() {
         return fishMapper.map(fishService.readAll());
     }
 
-    @GetMapping ("/fishes/cost")
-    Float cost(@RequestParam("fishName") String fishName, @RequestParam("fishCount") String fishCount) {
+    @GetMapping ("/cost")
+    Float cost(@RequestParam("fishName") @NotBlank String fishName,
+               @RequestParam("fishCount") @NotNull @Positive String fishCount) {
         return fishService.calcCost(fishName, Float.parseFloat(fishCount));
     }
 
